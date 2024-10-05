b6be1d640e582154e0c3361fddcb2ef763e22179
Alexander Belkin <belkin.alexander@gmail.com>
2024-10-05 18:05:31 +0200

    FEAT: add Math.floor to ProgressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -5,7 +5,7 @@ export const ProgressBar = () => {
     const progressElement = useRef(null);
 
     useEffect(() => {
-        const onScroll = () => progressElement.current.style.width = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100 + '%';
+        const onScroll = () => progressElement.current.style.width = Math.floor((window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100) + '%';
         window.addEventListener('scroll', onScroll);
 
         return () => window.removeEventListener('scroll', onScroll);
