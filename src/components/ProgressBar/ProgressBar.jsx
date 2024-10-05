930e92b8465c02be7904c324b4936f9f7552786e
Alexander Belkin <belkin.alexander@gmail.com>
2024-10-05 16:45:03 +0200

    FIX: ProgressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,21 +1,19 @@
 import { useRef, useEffect } from 'react';
-import ScrollProgress from './scrollProgress.js';
 import styles from './ProgressBar.module.css';
 
 export const ProgressBar = () => {
     const progressElement = useRef(null);
 
     useEffect(() => {
-        const progressObserver = new ScrollProgress((x, y) => {
-            progressElement.current.style.width = y * 100 + '%';
-        });
+        const onScroll = () => progressElement.current.style.width = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100 + '%';
+        window.addEventListener('scroll', onScroll);
 
-        return () => progressObserver.destroy();
+        return () => window.removeEventListener('scroll', onScroll);
     }, []);
 
     return (
         <>
-            <div ref={progressElement} className={styles['progress-bar']}></div>
+            <div ref={progressElement} className={styles['progress-bar']} />
         </>
     );
 }
