1c6b370299a24e1f6debe1358a2a9957f443614c
Alexander Belkin <belkin.alexander@gmail.com>
2024-10-09 20:06:31 +0200

    FIX: HW-4 ProgressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -2,22 +2,21 @@ import { useEffect, useState } from 'react';
 import styles from './ProgressBar.module.css';
 
 export const ProgressBar = () => {
-    const [progressElement, setProgressElement] = useState(null);
+    const [width, setWidth] = useState(0);
 
     useEffect(() => {
         const onScroll = () => {
-            if (progressElement) {
-                progressElement.style.width = Math.floor((window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100) + '%';
-            }
+            setWidth(Math.floor((window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100));
         };
+
         window.addEventListener('scroll', onScroll);
 
         return () => window.removeEventListener('scroll', onScroll);
-    }, [progressElement]);
+    }, []);
 
     return (
         <>
-            <div ref={setProgressElement} className={styles['progress-bar']} />
+            <div className={styles['progress-bar']} style={{ width: `${width}%` }}/>
         </>
     );
 }
